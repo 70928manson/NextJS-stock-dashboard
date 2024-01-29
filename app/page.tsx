82f85192b65a2cfe988f126c0cab015cefeed76d
70928manson <manson972@yahoo.com>
2024-01-29 15:42:10 +0800

    feat: implement MarketItem component for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
 import Image from "next/image";
+import MarketItem from "./components/home/MarketItem";
 
 export default function Home() {
   return (
-    <main className="flex min-h-main-content flex-col items-center p-12">
-      <div className="flex">
-        <div>
-          加權指數
-        </div>
-        <div>
-          淨值比
-        </div>
-        <div>
-          Vix
+    <main className="flex min-h-main-content flex-col items-center">
+      <div className="max-w-[1140px] w-[80%] m-home-market-info">
+        <div className="w-full overflow-hidden relative bg-white rounded shadow-home-market-info my-auto">
+          <div className="flex m-2">
+            <MarketItem title={"台股加權指數"} value={`17995.03`} misc={`-7.95 (-0.04%)`} status={`down`} />
+            <MarketItem title={"台股成交金額"} value={`2581.44億`} misc={`昨日3005.17億`} />
+            <MarketItem title={"台股股價淨值比"} value={`2.13倍`} misc={`昨日2.13倍`} />
+            <MarketItem title={"台股本益比"} value={`21.25倍`} misc={`昨日21.25倍`} />
+          </div>
+          <div>指數圖</div>
         </div>
       </div>
-      <div>指數圖</div>
       <div>
         產業熱力圖
       </div>
