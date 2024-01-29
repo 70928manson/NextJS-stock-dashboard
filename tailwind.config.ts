8840d89ad4bc96dbc259025e8d7882f5badfd5f0
70928manson <manson972@yahoo.com>
2024-01-29 10:12:26 +0800

    update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      minHeight: {
+        "main-content": "calc(100vh - 64px)"
+      }
     },
   },
+  
   plugins: [],
 };
 export default config;
