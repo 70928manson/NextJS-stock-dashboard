491f06e305e4dabce290f0b9d24a0c3cad0848a0
70928manson <manson972@yahoo.com>
2024-03-01 14:17:52 +0800

    feat: implement fetch TWSE and TPEX margin transactions at indicator page - 從臺灣證交所和櫃買中心取得融資融券資料

--- app/indicators/page.tsx
+++ app/indicators/page.tsx
@@ -1,13 +1,23 @@
+import { fetchTPEXMarginTransactions } from "../lib/TPEX/tpexAction";
+import { fetchTWSEMarginTransactions } from "../lib/TWSE/twseAction";
 import { fetchMxfMarketOi, fetchBigThreeMxf } from "../lib/indicatorAction";
 import { notFound } from 'next/navigation';
 
 export default async function Page() {
-    const [openInterest, bigThreeOpenInterest] = await Promise.all([
+    const today = new Date();
+    const yesterday = new Date(today.getTime() - (24 * 60 * 60 * 1000));
+    const yesterdayYear = yesterday.getFullYear();
+    const yesterdayMonth = String(yesterday.getMonth() + 1).padStart(2, "0");
+    const yesterdayDate = String(yesterday.getDate()).padStart(2, "0");
+
+    const [openInterest, TWSEMarginTransactions, TPEXMarginTransactions, bigThreeOpenInterest] = await Promise.all([
         fetchMxfMarketOi(),
-        fetchBigThreeMxf()
+        fetchTWSEMarginTransactions(`${yesterdayYear}-${yesterdayMonth}-${yesterdayDate}`),
+        fetchTPEXMarginTransactions(`${yesterdayYear}-${yesterdayMonth}-${yesterdayDate}`),
+        fetchBigThreeMxf(),
     ])
 
-    if (!openInterest || !bigThreeOpenInterest) {
+    if (!openInterest || !TWSEMarginTransactions || !TPEXMarginTransactions || !bigThreeOpenInterest) {
         notFound();
     }
 
@@ -19,21 +29,45 @@ export default async function Page() {
     const smallLongShortRation = (Math.round(smallClean / openInterest * 100 * 100) / 100).toFixed(2);
 
     return (
-        <main className="flex min-h-main-content flex-col items-center">
-            經濟指標: 散戶, 大戶, 法人, Fed利率
+        <main className="w-[80%] h-full mx-auto">
+            <p className="text-center">經濟指標: 散戶, 大戶, 法人, Fed利率</p>
             
-            <h3 className="text-lg p-2">三大法人買賣超</h3>
+            {/* <h3 className="text-lg p-2 font-bold">三大法人買賣超</h3> */}
 
-            <h2 className="text-2xl p-2">散戶</h2>
+            <div className="flex flex-col items-center">
+                <h2 className="text-2xl p-2 font-bold">散戶指標 - 融資融券餘額</h2>
+                <div className="flex justify-center">
+                    <div className="p-1">
+                        <h3 className="text-xl p-2 font-bold">集中市場</h3>
+                        <h4 className="text-lg p-2">融資今日餘額 (交易單位): {TWSEMarginTransactions.marginBalance}</h4>
+                        <h4 className="text-lg p-2">融資餘額增減 (交易單位): {TWSEMarginTransactions.marginBalanceChange}</h4>
+                        <h4 className="text-lg p-2">融資金額(仟元)-今日餘額: {TWSEMarginTransactions.marginBalanceValue}</h4>
+                        <h4 className="text-lg p-2">融資餘額增減(仟元): {TWSEMarginTransactions.marginBalanceValueChange}</h4>
+                        <h4 className="text-lg p-2">融券(交易單位)-今日餘額: {TWSEMarginTransactions.shortBalance}</h4>
+                        <h4 className="text-lg p-2">融券餘額增減(交易單位): {TWSEMarginTransactions.shortBalanceChange}</h4>
+                    </div>
+                    <div className="p-1">
+                        <h3 className="text-xl p-2 font-bold">櫃買中心</h3>
+                        <h4 className="text-lg p-2">融資今日餘額 (交易單位): {TPEXMarginTransactions.marginBalance}</h4>
+                        <h4 className="text-lg p-2">融資餘額增減 (交易單位): {TPEXMarginTransactions.marginBalanceChange}</h4>
+                        <h4 className="text-lg p-2">融資金額(仟元)-今日餘額: {TPEXMarginTransactions.marginBalanceValue}</h4>
+                        <h4 className="text-lg p-2">融資餘額增減(仟元): {TPEXMarginTransactions.marginBalanceValueChange}</h4>
+                        <h4 className="text-lg p-2">融券(交易單位)-今日餘額: {TPEXMarginTransactions.shortBalance}</h4>
+                        <h4 className="text-lg p-2">融券餘額增減(交易單位): {TPEXMarginTransactions.shortBalanceChange}</h4>
+                    </div>
+                </div>
+                <h2 className="text-2xl p-2 font-bold">散戶指標 - 小台散戶多空比</h2>
+                <div className="p-1">
+                    <div>
+                        <p className="p-2">散戶多空比 = 散戶淨部位 / 未沖銷契約量 * 100%</p>
+                        <h4 className="text-sm p-2">未沖銷契約量: {openInterest} </h4>
+                        <h4 className="text-sm p-2">三大法人多方未平倉: {bigThreeOpenInterest.long} </h4>
+                        <h4 className="text-sm p-2">三大法人空方未平倉: {bigThreeOpenInterest.short} </h4>
+                        <h4 className="text-sm p-2">小台散戶多空比: {smallLongShortRation} </h4>
+                    </div>
 
-            <h3 className="text-lg p-2">融資融券餘額: </h3>
-            
-            <h3 className="text-lg p-2">小台散戶多空比: {smallLongShortRation} </h3>
-            <h4 className="text-sm p-2">未沖銷契約量: {openInterest} </h4>
-            <h4 className="text-sm p-2">三大法人多方未平倉: {bigThreeOpenInterest.long} </h4>
-            <h4 className="text-sm p-2">三大法人空方未平倉: {bigThreeOpenInterest.short} </h4>
-            <p>散戶多空比 = 散戶淨部位 / 未沖銷契約量 * 100%</p>
-
+                </div>
+            </div>
         </main>
     );
 }
---
+++ app/lib/TPEX/tpexAction.ts
@@ -0,0 +1,72 @@
+'use server';
+
+// TPEX 證券櫃檯買賣中心
+
+import { DateTime } from 'luxon';
+import numeral from "numeral";
+
+// 取得櫃買市場融資融券餘額
+export async function fetchTPEXMarginTransactions(date: string) {
+    // 將 `date` 轉換成 `民國年/MM/dd` 格式
+    const dt = DateTime.fromISO(date);
+    const year = dt.get('year') - 1911;
+    const formattedDate = `${year}/${dt.toFormat('MM/dd')}`;
+
+    // 建立 URL 查詢參數
+    const query = new URLSearchParams({
+        l: 'zh-tw',       // 指定語系為正體中文
+        d: formattedDate, // 指定日期
+        o: 'json',        // 指定回應格式為 JSON
+    });
+    const url = `https://www.tpex.org.tw/web/stock/margin_trading/margin_balance/margin_bal_result.php?${query}`;
+
+    // 取得回應資料
+    const response = await fetch(url).then((res) => res.json());
+
+    // 若該日期非交易日或尚無成交資訊則回傳 null
+    if (!response) return null;
+
+    // 整理回應資料  data => numeral(data).value()
+    const data = [
+        ...response.tfootData_one,
+        ...response.tfootData_two
+    ].map((data: string) => numeral(data).value())   // 取出融資融券統計並轉為數字格式
+    .filter((data) => data) as number[];             // 移除 null 值, data 必為number[]
+
+    const [
+        marginBalancePrev,        // 融資(交易單位)-前日餘額
+        marginPurchase,           // 融資(交易單位)-買進
+        marginSale,               // 融資(交易單位)-賣出
+        cashRedemption,           // 融資(交易單位)-現金(券)償還
+        marginBalance,            // 融資(交易單位)-今日餘額
+        shortBalancePrev,         // 融券(交易單位)-前日餘額
+        shortCovering,            // 融券(交易單位)-買進
+        shortSale,                // 融券(交易單位)-賣出
+        stockRedemption,          // 融券(交易單位)-現金(券)償還
+        shortBalance,             // 融券(交易單位)-今日餘額
+        marginBalanceValuePrev,   // 融資金額(仟元)-前日餘額
+        marginPurchaseValue,      // 融資金額(仟元)-買進
+        marginSaleValue,          // 融資金額(仟元)-賣出
+        cashRedemptionValue,      // 融資金額(仟元)-現金(券)償還
+        marginBalanceValue,       // 融資金額(仟元)-今日餘額
+    ] = data;
+
+    // 計算融資餘額增減(交易單位)
+    const marginBalanceChange = marginBalance - marginBalancePrev;
+
+    // 計算融資餘額增減(仟元)
+    const marginBalanceValueChange = marginBalanceValue - marginBalanceValuePrev;
+
+    // 計算融券餘額增減(交易單位)
+    const shortBalanceChange = shortBalance - shortBalancePrev;
+
+    return {
+        date,
+        marginBalance,
+        marginBalanceChange,
+        marginBalanceValue,
+        marginBalanceValueChange,
+        shortBalance,
+        shortBalanceChange,
+    };
+}
