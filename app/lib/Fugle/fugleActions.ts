607d1337d6c4dd99f0f53dae7e43b4602ab59496
70928manson <manson972@yahoo.com>
2024-03-06 11:32:02 +0800

    feat: delete twse action in fugleActions

--- app/lib/Fugle/fugleActions.ts
+++ app/lib/Fugle/fugleActions.ts
@@ -6,51 +6,6 @@ import numeral from "numeral";
 
 // Fugle 富果API
 
-//透過證交所api 拿本益比 淨值比 殖利率
-export async function fetchTWSEBasicData(date: string, stockNo: string) {
-    // 將 `date` 轉換成 `yyyyMMdd` 格式
-    const formattedDate = DateTime.fromISO(date).toFormat('yyyyMMdd');
-
-    // 建立 URL 查詢參數
-    const query = new URLSearchParams({
-        date: formattedDate,  // 指定資料日期
-        stockNo: stockNo,     // 股票代號
-        response: 'json',     // 指定回應格式為 JSON
-    });
-
-    const url = `https://www.twse.com.tw/rwd/zh/afterTrading/BWIBBU?${query}`;
-
-    // 取得回應資料
-    const response = await fetch(url).then((res) => res.json());
-
-    // 若該日期非交易日或尚無成交資訊則回傳 null
-    if (!response) return null;
-
-    // 整理回應資料  => 這邊要改
-    const data: stockTWSEData[] = response.data.map((row: string[]) => {
-        // [ 日期, 殖利率, 股利年度, 本益比, 股價淨值比, 財報年/季 ]
-        const [dateA, ...values] = row;
-
-        const formatted = DateTime.fromISO(date).toFormat('yyyyMMdd');
-
-        const formattedDate = DateTime.fromFormat(formatted, 'yyyyMMdd').toISODate();
-
-        //轉為數字格式
-        const [dividenedYield, dividenedYear, PE, PB, financialQuarter] = values.map(value => numeral(value).value());
-
-        return {
-            date: formattedDate,
-            dividenedYield,
-            dividenedYear,
-            PE,
-            PB,
-        };
-    })
-    const length = data.length
-
-    return data[length - 1];
-};
-
 //富果API stock基本資料 拿g收盤價(最後成交價), 漲跌, 漲跌幅, 成交量 (金額)
 //今日行情
 export async function fetchFugleTodayData(stockNo: string) {
