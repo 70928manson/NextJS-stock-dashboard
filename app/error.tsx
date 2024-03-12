0ff74d5d7c5053cc7dc823039252ba21dc0bab04
70928manson <manson972@yahoo.com>
2024-03-12 15:59:10 +0800

    feat: implement route error boundary - 新增error.tsx做錯誤處理

---
+++ app/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Error() {
+    return (
+        <div className='absolute top-1/2 left-1/2 translate-x-[-50%] font-bold text-[30px]'>
+            發生問題，請稍後再試
+        </div>
+    );
+}
