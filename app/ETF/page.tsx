c48511d2c321ed4c51cadc1a63d8e483cba1faa2
70928manson <manson972@yahoo.com>
2024-03-19 16:28:26 +0800

    update: add dividend ETF jointly held shares at ETF page

--- app/ETF/page.tsx
+++ app/ETF/page.tsx
@@ -1,18 +1,26 @@
 import { fetchFugleTodayData } from "../lib/Fugle/fugleActions";
+import { getDownloads } from "../lib/Matchbox/matchboxActions";
 
 export default async function Page() {
 
     //fetchFugleTodayData
-    const [todayData] = await Promise.all([
-        fetchFugleTodayData('0050')
+    const [todayData, test] = await Promise.all([
+        fetchFugleTodayData('0050'),
+        getDownloads("00929")
     ]);
+
+     console.log("test", test);
+    
 
     return (
         <main>
             ETF觀測站
             基本資訊
-            高股息共同持股
 
+            old: 0056 00878 00919 00929 00713
+            new: 00939 00940
+            規模前5大高股息etf前10大共同持股
+            使用web scraper爬取MoneyDJ
             <div>
                 <h3>{todayData.name}</h3>
                 <p>{todayData.openPrice}</p>
---
+++ app/lib/Matchbox/matchboxActions.ts
@@ -0,0 +1,31 @@
+// 爬蟲開抓
+import { JSDOM } from 'jsdom';
+
+export async function getDownloads(etf: string = '0050') {
+    const res = await fetch(`https://matchbox.space/etf?stock=${etf}.TW`)
+
+    
+    const html = await res.text();
+    
+    const dom = new JSDOM(html); 
+
+    const document = dom.window.document;
+    
+    // 前十大持股 名稱
+    const tableBody = document.querySelectorAll(".v-progress-linear__content div div");
+    const aaa = document.querySelectorAll(".v-avatar.v-list-item__avatar span .v-chip__content");
+
+    const arr = [];
+
+    const length = tableBody.length;
+
+    for (let i=0; i < length; i++) {
+        arr.push({
+            name: tableBody[i].textContent,
+            percent: aaa[i].textContent
+        })
+    }
+
+    // // return tableBody
+     return arr
+}
