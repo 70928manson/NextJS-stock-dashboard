704cab276dd4bfebc3268a3ef837f6a82e34b0f9
Manson <manson972@yahoo.com>
2024-05-01 18:57:16 +0800

    feat: 新增登入註冊表單

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,32 @@
+import Link from "next/link";
+
 export default async function Page() {
 
     return (
-        <div>Login Page</div>
+        <div className="">
+            Login Page
+            {/* /登入表單 */}
+            <div className="grid place-items-center h-screen">
+                <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
+                    <h1 className="text-xl font-bold my-4">Enter the details</h1>
+
+                    <form className="flex flex-col gap-3">
+                        <input type="text" placeholder="Email" name="" id="" />
+                        <input type="text" placeholder="Password" name="" id="" />
+                        <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">
+                            Login
+                        </button>
+
+                        <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+                            Error message
+                        </div>
+
+                        <Link href={'/register'}>
+                            Don't have an account? <span className="underline">Register</span>
+                        </Link>
+                    </form>
+                </div>
+            </div>
+        </div>
     );
 }
